c6268349c9f5b0c750dbc54f2d6afdae1d225c7e
Александр Котляров <bony17@bk.ru>
2022-07-20 13:55:36 +0300

    Решил 19 задачку

---
+++ task19/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+void ShifterNumber(int num)
+{
+    int lastNum = num % 10;
+    int firstNum = num / 10000;
+    if (firstNum == lastNum)
+    {
+        lastNum = (num % 100) / 10;
+        firstNum = (num / 1000) % 10;
+        if (firstNum == lastNum) 
+        Console.WriteLine($"{num} -> ДА");  
+        else Console.WriteLine($"{num} -> Нет!"); 
+    }
+     else Console.WriteLine($"{num} -> Нет!");        
+    
+}
+ShifterNumber(number);
+
+
+
+
